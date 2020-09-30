e07f50a775be4f33699eeca88c79906b69898459
bali <bali@localhost.localdomain>
2020-09-30 18:47:23 +0300

    par1

--- azaz.R
+++ azaz.R
@@ -1,7 +1,13 @@
-x<-c(2,2,2,2,2)
-y<-c(2,2,2,2,2)
-z<-x+y
-z
-
-
 install.packages("tidyverse")
+library(tidyverse)
+
+
+df<-mtcars
+df[2:5, 5:7]<-NA
+head(df)
+summary(df)
+sum(is.na(df))
+colSums(is.na(df))
+rowSums(is.na(df))
+x<-as.integer(2.4)
+typeof(x)
